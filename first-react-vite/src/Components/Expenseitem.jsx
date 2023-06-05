b1941274265001062b34e2ed816b84b23e67857e
Eldar <eldarbotyr@mail.ru>
2023-06-04 22:47:08 -0400

    Introducing useState and small cosmetics change

--- first-react-vite/src/Components/Expenseitem.jsx
+++ first-react-vite/src/Components/Expenseitem.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+
 function ExpenseItem(props) {    
-    console.dir(props.expense)
+    const expense = props.expense;
+    const [amount, setAmount] = useState(expense.amount);
+    function clickHandler() {
+        setAmount((amount) => amount + 1);
+        console.dir(amount)
+    }
     return <div className="expense-item">
-        <h2 id="title">{props.expense.title}</h2>
-        <div id="date">{props.expense.date.toDateString()}</div>
-        <div id="amount">{props.expense.amount}</div>
+        <h2 id="title">{expense.title}</h2>
+        <div id="date">{expense.date.toDateString()}</div>
+        <div id="amount">{amount}</div>
+        <button onClick={clickHandler}>Up Price</button>
     </div>
 }
 export default ExpenseItem;
